80e720dada56f3b294f236a97dd1d504dbd118bf
Joe Cox <joecox@live.co.uk>
2021-01-31 04:22:05 +0000

    completed removed firebase code

--- client/src/App.js
+++ client/src/App.js
@@ -6,30 +6,30 @@ import ShoppingBasket from "pages/ShoppingBasket";
 
 import { Switch, Route } from "react-router-dom";
 
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setUser } from "./store/slices/user";
-import { auth } from "./firebase/config/firebase.config";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { setUser } from "./store/slices/user";
+// import { auth } from "./firebase/config/firebase.config";
 
 function App() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   // need to add hidden class on to loading while user doesn't exist
 
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        // User is signed in.
-        dispatch(
-          setUser({
-            name: user.displayName,
-            email: user.email,
-          })
-        );
-      } else {
-        // No user is signed in.
-      }
-    });
-  }, [dispatch]);
+  // useEffect(() => {
+  //   auth.onAuthStateChanged((user) => {
+  //     if (user) {
+  //       // User is signed in.
+  //       dispatch(
+  //         setUser({
+  //           name: user.displayName,
+  //           email: user.email,
+  //         })
+  //       );
+  //     } else {
+  //       // No user is signed in.
+  //     }
+  //   });
+  // }, [dispatch]);
 
   return (
     <Layout>
